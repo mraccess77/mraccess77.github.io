e228c44f69f9fe9c1e9d894f408a88b2d66a5d73
mraccess77 <jon.avila@ssbbartgroup.com>
2015-08-30 19:41:58 -0400

    Added enhanceFocus.js

---
+++ favlets/enhanceFocus.js
@@ -0,0 +1,8 @@
+(function() {
+ var style = document.createElement('style');
+ style.type = 'text/css';
+ style.appendChild(document.createTextNode());
+ document.head.appendChild(style);
+ style.sheet.insertRule('*:focus { outline: red solid thick !important; } ',-1);
+})();
+
