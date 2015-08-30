ee4968096aa916124a3aca0d0ccdc2bb7a7578de
mraccess77 <jon.avila@ssbbartgroup.com>
2015-08-30 19:46:43 -0400

    fixed error.

--- favlets/enhanceFocus.js
+++ favlets/enhanceFocus.js
@@ -1,7 +1,7 @@
 (function() {
  var style = document.createElement('style');
  style.type = 'text/css';
- style.appendChild(document.createTextNode());
+ //style.appendChild(document.createTextNode());
  document.head.appendChild(style);
  style.sheet.insertRule('*:focus { outline: red solid thick !important; } ',-1);
 })();
