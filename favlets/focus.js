749ff16e9e7334865f06a4fc8769677cee3b66ef
Jonathan Avila <jon.avila@ssbbartgroup.com>
2017-01-26 00:48:41 -0500

    updated focus.js

--- favlets/focus.js
+++ favlets/focus.js
@@ -1,13 +1,12 @@
     
-     var overlay;
-	 var lostFocus;
-	 var errorsObj;
+     //var overlay;
+	 var focusObj;
 	 
-	 function traverseFrames(doc) {
+	 function traverseFrames(doc, focusObj) {
 
 	// check for sr-only class in current document and then check it's frames
 
-	initHelper();
+	initHelper(doc, focusObj);
 
 	// go through for each frame's document if there are any frames
 	var frametypes= ['frame','iframe'];
@@ -15,7 +14,7 @@
 		var myframes=doc.getElementsByTagName(frametypes[i]);
 		for (var z=0;z<myframes.length;z++) {
 			try {
-		    traverseFrames(myframes[z].contentWindow.document);
+		    traverseFrames(myframes[z].contentWindow.document, focusObj);
 		  } catch(e) {
 			}
 		}
@@ -24,38 +23,63 @@
 	}
 
       function rect(elem) {
-	    console.log("jon");
+	    console.log("rect");
 		
 	    elemTarg = elem.target || elem;
 		if (elem) {
+			console.log("element");
 	     	var cX = elemTarg.getBoundingClientRect().left;
 			var cY = elemTarg.getBoundingClientRect().top;
 			var rectH = elemTarg.getBoundingClientRect().height;
 			var rectW = elemTarg.getBoundingClientRect().width;
-			 overlay.style.top = cY;
-			 overlay.style.left = cX;
-			 overlay.style.height = rectH;
-			 overlay.style.width = rectW;
+			var overlay = elemTarg.ownerDocument.getElementById("SSB");
+			
+			if (cX < 0) {cX = 0};
+			if (cY < 0) {cY = 0};
+			if (rectH==0) {rectH = 3};
+			if (rectW==0) {rectW = 3};
+			 overlay.style.top = parseInt(cY).toString() + "px";
+			 overlay.style.left = parseInt(cX).toString() + "px";
+			 overlay.style.height = parseInt(rectH).toString() + "px";
+			 overlay.style.width = parseInt(rectW).toString() + "px";
+			 if (cX ==0 || cY == 0) {
+			   //overlay.appendChild(elemTarg.cloneNode(true));
+			   overlay.textContent = elemTarg.tagName;
+			 }
+			console.log(overlay.style.left);
+			console.log(overlay.style.top);
+			console.log(overlay.style.height);
+			console.log(overlay.style.width);
 		 }
 	 }
-	 function rectRemove() {
+	 function rectRemove(elem) {
+		 console.log("rect remove");
+		 elemTarg = elem.target;
+		 var overlay = elemTarg.ownerDocument.getElementById("SSB");
 	 	 overlay.style.top = -10;
 		 overlay.style.left = -10;
 		 overlay.style.height = 1;
 		 overlay.style.width = 1;
-		 lostFocus = true;
+		 //overlay.innerHTML = ''; // wipe out any clones
+		 
+		 overlay.setAttribute("data-lostFocus","true");
 	 }
 	  function init() {
-	    traverseFrames(document)
+		console.log("init");
+	    traverseFrames(document, focusObj);
 	  }
 	  
-	  function initHelper() {
-	     overlay = document.createElement("div");
+	  function initHelper(doc, focusObj) {
+		  console.log("init helper");
+	     var overlay = doc.createElement("div");
 		 overlay.style.position = "fixed";
-		 overlay.style.border = "thin solid red";
-	     document.body.appendChild(overlay);
-		 document.addEventListener("blur",rectRemove, true);
-		 document.addEventListener("focus",function() {lostFocus = false; rect(event);}, true);
-		 document.addEventListener('scroll',function() {if (!lostFocus) {rect(document.activeElement);}},true);
+		 overlay.style.border = "medium solid red";
+		 overlay.style.zIndex = "9999";
+		 overlay.id = "SSB";
+		 overlay.setAttribute("data-lostFocus","false");
+	     doc.body.appendChild(overlay);
+		 doc.addEventListener("blur",rectRemove, true);
+		 doc.addEventListener("focus",function() {doc.getElementById("SSB").setAttribute("data-lostFocus","false"); rect(event);}, true);
+		 doc.addEventListener('scroll',function() {if (doc.getElementById("SSB").getAttribute("data-lostFocus") == "false") {rect(doc.activeElement);}},true);
 	  }
      init();
