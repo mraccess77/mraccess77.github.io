09e41640a4c16b5fa9a23576de6a2237ca597b54
Jonathan Avila <jon.avila@ssbbartgroup.com>
2018-05-29 09:30:58 -0400

    add text spacing favlet

---
+++ favlets/textSpacing.js
@@ -0,0 +1,16 @@
+(function() {
+ var style = document.createElement('style');
+ style.type = 'text/css';
+ //style.appendChild(document.createTextNode());
+ document.head.appendChild(style);
+ style.sheet.insertRule('* {
+ line-height: 1.5 !important;
+ letter-spacing: 0.12em !important;
+ word-spacing: 0.16em !important;
+}
+p{
+ margin-bottom: 2em !important;
+} 
+',  style.sheet.cssRules.length);
+})();
+
