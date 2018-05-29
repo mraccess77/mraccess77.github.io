dca91d1444d73e74c81dd660819f00a5a8b01f8a
Jonathan Avila <jon.avila@ssbbartgroup.com>
2018-05-29 10:16:36 -0400

    split out css rules for text spacing

--- favlets/textSpacing.js
+++ favlets/textSpacing.js
@@ -3,6 +3,10 @@
  style.type = 'text/css';
  //style.appendChild(document.createTextNode());
  document.head.appendChild(style);
- style.sheet.insertRule('*{line-height:1.5 !important; letter-spacing: 0.12em !important; word-spacing: 0.16em !important;} p{margin-bottom: 2em !important;}', style.sheet.cssRules.length);
+ style.sheet.insertRule('*{line-height:1.5 !important; letter-spacing: 0.12em !important; word-spacing: 0.16em !important;}', style.sheet.cssRules.length);
+ style.sheet.insertRule('p{margin-bottom: 2em !important;}', style.sheet.cssRules.length);
+ 
+
+  
 })();
 
