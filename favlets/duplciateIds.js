ee97319a4a01f09528a18f18a530c649e9535c46
Jonathan Avila <jon.avila@ssbbartgroup.com>
2018-09-01 20:42:40 -0400

    add duplciate ids js

---
+++ favlets/duplciateIds.js
@@ -0,0 +1,23 @@
+function runDuplicateIds() {
+	var col = document.querySelectorAll("[id]");
+	//alert(find_duplicate_ids(Array.prototype.slice.call( col, 0 )).length);
+	//
+	var results=[];			
+	find_duplicate_ids(col, results);
+	var strResults = results.length+" elements with duplicate ids"+"\n";
+	
+	results.forEach(function(item) {
+		 strResults = strResults + item.outerHTML + "\n"
+	});
+	alert(strResults);
+}
+		
+function find_duplicate_ids(arIDs, results) {
+	 arIDs.forEach(function (item) {
+			ids = document.querySelectorAll('[id="'+item.id+'"]');
+			 if (ids.length>1)
+					results.push(item);
+	 });
+}
+runDuplicateIds();
+		
