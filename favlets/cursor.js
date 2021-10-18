746dde97f16c8bc72354d222d02232a192319a88
Jonathan Avila <jon.avila@ssbbartgroup.com>
2021-10-18 07:43:32 -0400

    Update cursor.js

--- favlets/cursor.js
+++ favlets/cursor.js
@@ -1,9 +1,9 @@
 var myStyle = document.createElement('style');
 document.head.appendChild(myStyle);
 myStyle.sheet.insertRule(".cursor { border: 1px dotted black; width:24px; height: 24px; position: fixed; cursor: none;}", 0);
-document.addEventListener("DOMContentLoaded",setup);
 var d = document.createElement("div");
 d.className = "cursor";
+setup();
 
 function setup() {
 	document.body.appendChild(d);
