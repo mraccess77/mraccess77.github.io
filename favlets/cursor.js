7883711d0ee51ab0b05c8a0582dd3f0727b830e6
Jonathan Avila <jon.avila@ssbbartgroup.com>
2021-11-12 22:02:34 -0500

    Update cursor.js
    changed fixed to absolute positioning

--- favlets/cursor.js
+++ favlets/cursor.js
@@ -1,6 +1,6 @@
 var myStyle = document.createElement('style');
 document.head.appendChild(myStyle);
-myStyle.sheet.insertRule(".cursor { border: 1px dotted black; width:24px; height: 24px; position: fixed; cursor: none;}", 0);
+myStyle.sheet.insertRule(".cursor { border: 1px dotted black; width:24px; height: 24px; position: absolute; cursor: none;}", 0);
 var d = document.createElement("div");
 d.className = "cursor";
 setup();
