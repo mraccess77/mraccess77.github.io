ded01c1a0bf3e784090b1543b211d83a2929842b
Jonathan Avila <jon.avila@ssbbartgroup.com>
2023-07-21 13:39:32 -0400

    Added 24 diameter circle.

---
+++ favlets/circle.js
@@ -0,0 +1,15 @@
+var myStyle = document.createElement('style');
+document.head.appendChild(myStyle);
+myStyle.sheet.insertRule(".cursor { border: 1px dotted black; width:24px; height: 24px; border-radius: 50%; position: absolute; pointer-events: none !important; cursor: none !important;}", 0);
+var d = document.createElement("div");
+d.className = "cursor";
+setup();
+
+function setup() {
+	document.body.appendChild(d);
+	window.addEventListener("mousemove",cursor);
+}
+function cursor(e) {
+    d.style.left = e.pageX + "px";
+	d.style.top = e.pageY + "px";
+}
