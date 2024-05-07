ed029c525a2ab15a5b9b03e86d82da87f8763388
Miguel <123752096+miguel-nova-credit@users.noreply.github.com>
2024-05-07 16:26:40 -0300

    Update textSpacing.js to also update iframe's css

--- favlets/textSpacing.js
+++ favlets/textSpacing.js
@@ -1,12 +1,15 @@
 (function() {
  var style = document.createElement('style');
  style.type = 'text/css';
- //style.appendChild(document.createTextNode());
  document.head.appendChild(style);
  style.sheet.insertRule('*{line-height:1.5 !important; letter-spacing: 0.12em !important; word-spacing: 0.16em !important;}', style.sheet.cssRules.length);
  style.sheet.insertRule('p{margin-bottom: 2em !important;}', style.sheet.cssRules.length);
+
+ var iframe = document.querySelector("iframe").contentDocument;
+ style = iframe.createElement('style');
+ style.type = 'text/css';
+ iframe.head.appendChild(style);
+ style.sheet.insertRule('*{line-height:1.5 !important; letter-spacing: 0.12em !important; word-spacing: 0.16em !important;}', style.sheet.cssRules.length);
+ style.sheet.insertRule('p{margin-bottom: 2em !important;}', style.sheet.cssRules.length);
  
-
-  
 })();
-
